4c8bac8e5d713c63262c5ba1078b5946dcc89388
Martynas Kazakevicius <martynas.kazakevicius@visma.com>
2021-09-10 14:56:06 +0300

    Added base html part

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <header>
+            <div className="logo">
+                <h3>LOGO</h3>
+            </div>
+            <div>
+                <ul className="navigation-list">
+                    <li className="navigation-item">Search</li>
+                    <li className="navigation-item">Login</li>
+                </ul>
+            </div>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+            <p>e-shop 2021</p>
+        </footer>
     </div>
   );
 }
